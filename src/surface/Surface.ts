4c814bd8dcf2de5f90f7273ffd051ccde1e9d50c
Harshal Patil <harshal.rp@gmail.com>
2019-05-27 00:38:25 +0530

    Surface refactoring

--- src/surface/Surface.ts
+++ src/surface/Surface.ts
@@ -1,37 +1,34 @@
 import { Overlay } from './Overlay';
 
 interface Surface {
-  readonly layer: number;
-  readonly overlay: HTMLElement;
+  layer: number;
+  overlay: Overlay;
+
+  show: () => void;
+  dismiss: () => void;
+  children: (nodes: HTMLElement[]) => void;
 }
 
 // Array `surfaces` is a queue acting as a singleton and thus mutable.
 const surfaces: Surface[] = [];
-const surfaceId = Symbol();
 
 export function create() {
 
-  // 1. Create an overlay component
-  // 2. Take optional nodes during creation.
-  // 3. Return a data structure that will enable
-  //    - Showing the overlay on the page
-  //    - Removing the overlay
-  //    - Change child nodes
-  // 4. Destroy the overlay
-
   const overlay = document.createElement('wf-overlay') as Overlay;
 
-  const controller = {
-    show, dismiss, children,
-    [surfaceId]: null as any as Surface
+  const surface: Surface = {
+    layer: 0,
+    overlay,
+    show, dismiss, children
   };
+
 
   function show() {
 
     const layer = surfaces.length === 0 ? 100 : surfaces[surfaces.length - 1].layer + 1;
-    const surface: Surface = { layer, overlay };
 
-    controller[surfaceId] = surface;
+    surface.layer = layer;
+
     surfaces.push(surface);
 
     // Set to the created overlay
@@ -42,9 +39,9 @@ export function create() {
   };
 
   function dismiss() {
+
     document.body.removeChild(overlay);
 
-    const surface = controller[surfaceId];
     const index = surfaces.indexOf(surface);
 
     if (index > -1) {
@@ -56,6 +53,5 @@ export function create() {
     overlay.nodes = nodes;
   }
 
-  return controller;
-
+  return surface;
 }
