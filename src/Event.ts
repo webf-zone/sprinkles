85f555fd63b8a9323700653fe5c1557ce00ee6c3
Harshal Patil <harshal.rp@gmail.com>
2019-06-05 23:46:44 +0530

    Refactor event handling utility

--- src/Event.ts
+++ src/Event.ts
@@ -14,36 +14,53 @@ export function listenResize() {
 
 export function makeThrottler(eventName: string, elm: HTMLElement | Document | Window = window): EventMgr {
 
-  let handler: undefined | ((e: Event) => void);
   let pendingWork: number = 0;
+  const mgr = listen(eventName, elm);
 
-  const innerHandler = (e: Event) => {
+  function decorateHandler(handler: (e: Event) => void) {
 
-    if (handler) {
-
+    return function innerHandler(e: Event) {
       if (pendingWork !== 0) {
         window.cancelAnimationFrame(pendingWork);
       }
 
       // Throttle scroll event
       pendingWork = window.requestAnimationFrame(() => {
-        handler!(e);
+        handler(e);
         pendingWork = 0;
       });
-    }
+    };
+
+  }
+
+  const on = (handler: (e: Event) => void) => {
+    mgr.on(decorateHandler(handler));
   };
+
+  const off = () => {
+    window.cancelAnimationFrame(pendingWork);
+    mgr.off();
+  }
+
+  return { on, off };
+}
+
+export function listen(eventName: string, elm: HTMLElement | Document | Window = window): EventMgr {
+
+  let handler: undefined | ((e: Event) => void);
 
   const on = (eventHandler: (e: Event) => void) => {
     // Only one event handler at a time.
     if (!handler) {
       handler = eventHandler;
-      elm.addEventListener(eventName, innerHandler);
+      elm.addEventListener(eventName, handler);
+    } else {
+      console.warn('Attempt to add multiple event handler');
     }
   };
 
   const off = () => {
     if (handler) {
-      window.cancelAnimationFrame(pendingWork);
       elm.removeEventListener(eventName, handler);
       handler = undefined;
     }
